c1702d7574dd1eb84425407f88c2b76439de4541
HenryHerzfeld <herzfeld2@gmail.com>
2018-10-26 21:45:59 -0400

    change stream

--- dcim/stream.py
+++ dcim/stream.py
@@ -14,7 +14,7 @@ class StreamEngine:
             port=6379,
             db=0
         )
-        self.stream = db.Stream('stream1')
+        self.stream = db.Stream('stream2')
 
     def add(self, data):
         for item in data:
