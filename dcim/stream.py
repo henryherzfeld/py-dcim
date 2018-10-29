989903dd32699615be32b607d142286fe169b47b
HenryHerzfeld <herzfeld2@gmail.com>
2018-10-29 11:19:32 -0400

    add password authentication to redis server

--- dcim/stream.py
+++ dcim/stream.py
@@ -8,10 +8,12 @@ class StreamEngine:
     def __init__(self):
         stream_config = get_config('db')
         host = stream_config['DB_HOST']
+        password = stream_config['DB_PASS']
 
         db = Database(
             host=host,
             port=6379,
+            password=password,
             db=0
         )
         self.stream = db.Stream('stream2')
