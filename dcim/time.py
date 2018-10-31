cfcc48106bc8888614ae169fe52cfdd8097ff256
HenryHerzfeld <herzfeld2@gmail.com>
2018-10-31 00:45:21 -0400

    refactor build for sensor

--- dcim/time.py
+++ dcim/time.py
@@ -4,7 +4,7 @@ from dcim.configuration import get_config
 
 # wait for specified (conf.yaml) interval value
 def wait(start):
-    interval = get_config('chron')
-    print('waiting {0} seconds'.format(int(interval)-(time()-start)))
+    interval = get_config('chron')['COLL_INTERVAL']
+    print('waiting {0} seconds'.format(interval-(time()-start)))
     while time() - start < interval:
         sleep(.1)
